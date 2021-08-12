2d6c11680608af02b778390954e53c7484902893
bhd-corp <85958379+bhd-corp@users.noreply.github.com>
2021-08-12 21:21:56 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,627 @@
+document.getElementById('play').onclick = function() {
+let y = document.getElementById('lev');
+let rs = document.getElementById('q');
+y.style.display = 'block';
+rs.style.display = 'none';
+let rsq = document.getElementById('menu');
+rsq.style.display = 'none';
+let w = 0;
+// level1 //
+let bv = document.getElementById('chr');
+bv.addEventListener('click', function(){
+	let bcx = document.getElementById('lev');
+	bcx.style.display = 'none';
+	let mc = document.getElementById('levz');
+	mc.style.display = 'block';
+});
+let jk = document.getElementById('chl');
+jk.addEventListener('click', function(){
+	let bcx = document.getElementById('lev');
+	bcx.style.display = 'block';
+	let mc = document.getElementById('levz');
+	mc.style.display = 'none';
+});
+let bvq = document.getElementById('uvb');
+bvq.addEventListener('click', function(){
+	let bcx = document.getElementById('levz');
+	bcx.style.display = 'none';
+	let mcx = document.getElementById('menu');
+	mcx.style.display = 'block';
+	let mc = document.getElementById('q');
+	mc.style.display = 'none';
+});
+let i = document.getElementById('rh');
+i.addEventListener('click', function(){
+	let n = document.getElementById('in').value;
+	if (n === '728') {
+		let hd = document.getElementById('lvlx');
+		hd.style.display = 'block';
+		let p = document.getElementById('q');
+		p.style.opacity = '0.3';
+		hd.style.top = '-480px';
+		let hdp = document.getElementById('pas');
+		hdp.style.display = 'none';
+	} 
+});
+
+	let k = document.getElementById('ne');
+	k.addEventListener('click', function(){
+		let g = document.getElementById('lvlx');
+		g.style.display = 'none';
+		location.reload();
+	});
+	let ua = document.getElementById('u');
+	ua.addEventListener('click', function(){
+		let ga = document.getElementById('lev');
+		ga.style.display = 'none';
+		let gf = document.getElementById('menu');
+		gf.style.display = 'block';
+	});
+	let ual = document.getElementById('l1');
+	ual.addEventListener('click', function(){
+		let ga = document.getElementById('lev');
+		ga.style.display = 'none';
+		let gf = document.getElementById('menu');
+		gf.style.display = 'none';
+		let kj = document.getElementById('q');
+		kj.style.display = 'block';
+		setTimeout( function(){
+	let h1 = document.getElementById('h1');
+	h1.style.opacity = '1';
+	h1.style.animation = 'wer 2s linear';
+	h1.innerHTML = '7';
+	setTimeout(function(){
+		h1.style.animation = 'none';
+		h1.style.opacity = '0';
+	}, 2000);
+}, 1000);
+setTimeout( function(){
+	let h1 = document.getElementById('h1');
+	h1.style.opacity = '1';
+	h1.style.animation = 'r 2s linear';
+	h1.innerHTML = '2';
+	setTimeout( function(){
+		h1.style.animation = 'none';
+		h1.style.opacity = '0';
+	}, 1900);
+}, 4000);
+setTimeout( function(){
+	let h1 = document.getElementById('h1');
+	h1.style.opacity = '1';
+	h1.style.animation = 'wr 2s linear';
+	h1.innerHTML = '8';
+	setTimeout( function(){
+		h1.style.animation = 'none';
+		h1.style.opacity = '0';
+	}, 2000);
+}, 7000);
+let o = document.getElementById('rh');
+let r = document.getElementById('h5');
+setTimeout( function(){
+	h1.style.display = 'none';
+	let hb = document.getElementById('k');
+	hb.style.display = 'block';
+	let ino = document.getElementById('in');
+	ino.style.display = 'block';
+	o.style.display = 'block';
+	let ina = document.getElementById('in2');
+	ina.style.display = 'none';
+	r.style.display = 'block';
+}, 10000);
+	});
+}
+
+let ww = document.getElementById('we');
+ww.addEventListener('click', function(){
+	let de = document.getElementById('pas');
+	de.style.display = 'none';
+	let jj = document.getElementById('menu');
+	jj.style.display = 'block';
+	let j = document.getElementById('q');
+	j.style.display = 'none';
+	j.style.opacity = '1';
+});
+
+// Level1
+let ual = document.getElementById('l1');
+let qg = document.getElementById('l15');
+
+	ual.addEventListener('click', function(){
+		qg.innerHTML = 'Level: 1/10';
+		let ga = document.getElementById('lev');
+		ga.style.display = 'none';
+		let gf = document.getElementById('menu');
+		gf.style.display = 'none';
+		let kj = document.getElementById('q');
+		kj.style.display = 'block';
+		setTimeout( function(){
+	let h1 = document.getElementById('h1');
+	h1.style.opacity = '1';
+	h1.style.animation = 'wer 2s linear';
+	h1.innerHTML = '7';
+	setTimeout(function(){
+		h1.style.animation = 'none';
+		h1.style.opacity = '0';
+	}, 2000);
+}, 1000);
+setTimeout( function(){
+	let h1 = document.getElementById('h1');
+	h1.style.opacity = '1';
+	h1.style.animation = 'r 2s linear';
+	h1.innerHTML = '2';
+	setTimeout( function(){
+		h1.style.animation = 'none';
+		h1.style.opacity = '0';
+	}, 1900);
+}, 4000);
+setTimeout( function(){
+	let h1 = document.getElementById('h1');
+	h1.style.opacity = '1';
+	h1.style.animation = 'wr 2s linear';
+	h1.innerHTML = '8';
+	setTimeout( function(){
+		h1.style.animation = 'none';
+		h1.style.opacity = '0';
+	}, 2000);
+}, 7000);
+let o = document.getElementById('rh');
+let r = document.getElementById('h5');
+setTimeout( function(){
+	h1.style.display = 'none';
+	let hb = document.getElementById('k');
+	hb.style.display = 'block';
+	let ino = document.getElementById('in');
+	ino.style.display = 'block';
+	o.style.display = 'block';
+	r.style.display = 'block';
+}, 10000);
+	});
+
+let pause = document.getElementById('pause');
+pause.addEventListener('click', function(){
+	let pau = document.getElementById('pas');
+	pau.style.display = 'block';
+	pau.style.top = '-485px';
+	let bb = document.getElementById('q');
+	bb.style.opacity = '0.3';
+});
+
+// level2//
+let ualo = document.getElementById('l2');
+let gg = document.getElementById('l15');
+
+	ualo.addEventListener('click', function(){
+		gg.innerHTML = 'Level: 2/10';
+		let ga = document.getElementById('lev');
+		ga.style.display = 'none';
+		let gf = document.getElementById('menu');
+		gf.style.display = 'none';
+		let kj = document.getElementById('q');
+		kj.style.display = 'block';
+		setTimeout( function(){
+	let h1 = document.getElementById('h1');
+	h1.style.opacity = '1';
+	h1.style.animation = 'wer 2s linear';
+	h1.innerHTML = '35';
+	setTimeout(function(){
+		h1.style.animation = 'none';
+		h1.style.opacity = '0';
+	}, 2000);
+}, 1000);
+setTimeout( function(){
+	let h1 = document.getElementById('h1');
+	h1.style.opacity = '6';
+	h1.style.animation = 'r 2s linear';
+	h1.innerHTML = '66';
+	setTimeout( function(){
+		h1.style.animation = 'none';
+		h1.style.opacity = '0';
+	}, 1900);
+}, 4000);
+setTimeout( function(){
+	let h1 = document.getElementById('h1');
+	h1.style.opacity = '1';
+	h1.style.animation = 'wr 2s linear';
+	h1.innerHTML = '13';
+	setTimeout( function(){
+		h1.style.animation = 'none';
+		h1.style.opacity = '0';
+	}, 2000);
+}, 7000);
+let o = document.getElementById('rh');
+let r = document.getElementById('h5');
+setTimeout( function(){
+	h1.style.display = 'none';
+	let hb = document.getElementById('k');
+	hb.style.display = 'block';
+	let inod = document.getElementById('in2');
+	inod.style.display = 'block';
+	let ins = document.getElementById('in');
+	o.style.display = 'block';
+	ins.style.display = 'none';
+	r.style.display = 'block';
+}, 10000);
+	});
+	let s = document.getElementById('rh');	
+s.addEventListener('click', function(){
+	let nvx = document.getElementById('in2').value;
+	if (nvx === '356613') {
+		let hdb = document.getElementById('lvlx');
+		hdb.style.display = 'block';
+		let p = document.getElementById('q');
+		p.style.opacity = '0.3';
+		hdb.style.top = '-480px';
+		let hdp = document.getElementById('pas');
+		hdp.style.display = 'none';
+	}
+});
+
+
+
+
+// level3 // 
+let ur = document.getElementById('l3');
+let mmq = document.getElementById('l15');
+	ur.addEventListener('click', function(){
+		mmq.innerHTML = 'Level: 3/10';
+		let ga = document.getElementById('lev');
+		ga.style.display = 'none';
+		let gf = document.getElementById('menu');
+		gf.style.display = 'none';
+		let kj = document.getElementById('q');
+		kj.style.display = 'block';
+		setTimeout( function(){
+	let h1 = document.getElementById('h1');
+	h1.style.opacity = '1';
+	h1.style.animation = 'wer 2s linear';
+	h1.innerHTML = '67';
+	setTimeout(function(){
+		h1.style.animation = 'none';
+		h1.style.opacity = '0';
+	}, 2000);
+}, 1000);
+setTimeout( function(){
+	let h1 = document.getElementById('h1');
+	h1.style.opacity = '6';
+	h1.style.animation = 'r 2s linear';
+	h1.innerHTML = '94';
+	setTimeout( function(){
+		h1.style.animation = 'none';
+		h1.style.opacity = '0';
+	}, 1900);
+}, 4000);
+setTimeout( function(){
+	let h1 = document.getElementById('h1');
+	h1.style.opacity = '1';
+	h1.style.animation = 'wr 2s linear';
+	h1.innerHTML = '469';
+	setTimeout( function(){
+		h1.style.animation = 'none';
+		h1.style.opacity = '0';
+	}, 2000);
+}, 7000);
+let o = document.getElementById('rh');
+let r = document.getElementById('h5');
+setTimeout( function(){
+	h1.style.display = 'none';
+	let hb = document.getElementById('k');
+	hb.style.display = 'block';
+	let inod = document.getElementById('in2');
+	inod.style.display = 'block';
+	let ins = document.getElementById('in');
+	o.style.display = 'block';
+	ins.style.display = 'none';
+	r.style.display = 'block';
+}, 10000);
+	});
+	let m = document.getElementById('rh');	
+m.addEventListener('click', function(){
+	let nvx = document.getElementById('in2').value;
+	if (nvx === '6794469') {
+		let hdb = document.getElementById('lvlx');
+		hdb.style.display = 'block';
+		let p = document.getElementById('q');
+		p.style.opacity = '0.3';
+		hdb.style.top = '-480px';
+		let hdp = document.getElementById('pas');
+		hdp.style.display = 'none';
+	}
+});
+
+
+
+
+// level4
+
+let urw = document.getElementById('l4');
+let mmqw = document.getElementById('l15');
+
+	urw.addEventListener('click', function(){
+		let ga = document.getElementById('lev');
+		mmqw.innerHTML = 'Level: 4/10';
+		ga.style.display = 'none';
+		let gf = document.getElementById('menu');
+		gf.style.display = 'none';
+		let kj = document.getElementById('q');
+		kj.style.display = 'block';
+		setTimeout( function(){
+	let h1 = document.getElementById('h1');
+	h1.style.opacity = '1';
+	h1.style.animation = 'wer 2s linear';
+	h1.innerHTML = '67';
+	setTimeout(function(){
+		h1.style.animation = 'none';
+		h1.style.opacity = '0';
+	}, 2000);
+}, 1000);
+setTimeout( function(){
+	let h1 = document.getElementById('h1');
+	h1.style.opacity = '6';
+	h1.style.animation = 'r 2s linear';
+	h1.innerHTML = '94';
+	setTimeout( function(){
+		h1.style.animation = 'none';
+		h1.style.opacity = '0';
+	}, 1900);
+}, 4000);
+setTimeout( function(){
+	let h1 = document.getElementById('h1');
+	h1.style.opacity = '1';
+	h1.style.animation = 'wr 2s linear';
+	h1.innerHTML = '469';
+	setTimeout( function(){
+		h1.style.animation = 'none';
+		h1.style.opacity = '0';
+	}, 2000);
+}, 7000);
+let o = document.getElementById('rh');
+let r = document.getElementById('h5');
+setTimeout( function(){
+	h1.style.display = 'none';
+	let hb = document.getElementById('k');
+	hb.style.display = 'block';
+	let inod = document.getElementById('in2');
+	inod.style.display = 'block';
+	let ins = document.getElementById('in');
+	o.style.display = 'block';
+	ins.style.display = 'none';
+	r.style.display = 'block';
+}, 10000);
+	});
+	let mw = document.getElementById('rh');	
+mw.addEventListener('click', function(){
+	let nvx = document.getElementById('in2').value;
+	if (nvx === '6794469') {
+		let hdb = document.getElementById('lvlx');
+		hdb.style.display = 'block';
+		let p = document.getElementById('q');
+		p.style.opacity = '0.3';
+		hdb.style.top = '-480px';
+		let hdp = document.getElementById('pas');
+		hdp.style.display = 'none';
+	}
+});
+
+
+// level5//
+
+let urwx = document.getElementById('l5');
+let mmqz = document.getElementById('l15');
+
+	urwx.addEventListener('click', function(){
+		let ga = document.getElementById('lev');
+		mmqz.innerHTML = 'Level: 5/10';
+		ga.style.display = 'none';
+		let gf = document.getElementById('menu');
+		gf.style.display = 'none';
+		let kj = document.getElementById('q');
+		kj.style.display = 'block';
+		setTimeout( function(){
+	let h1 = document.getElementById('h1');
+	h1.style.opacity = '1';
+	h1.style.animation = 'wer 2s linear';
+	h1.innerHTML = '449';
+	setTimeout(function(){
+		h1.style.animation = 'none';
+		h1.style.opacity = '0';
+	}, 2000);
+}, 1000);
+setTimeout( function(){
+	let h1 = document.getElementById('h1');
+	h1.style.opacity = '6';
+	h1.style.animation = 'r 2s linear';
+	h1.innerHTML = '392';
+	setTimeout( function(){
+		h1.style.animation = 'none';
+		h1.style.opacity = '0';
+	}, 1900);
+}, 4000);
+setTimeout( function(){
+	let h1 = document.getElementById('h1');
+	h1.style.opacity = '1';
+	h1.style.animation = 'wr 2s linear';
+	h1.innerHTML = '671';
+	setTimeout( function(){
+		h1.style.animation = 'none';
+		h1.style.opacity = '0';
+	}, 2000);
+}, 7000);
+let o = document.getElementById('rh');
+let r = document.getElementById('h5');
+setTimeout( function(){
+	h1.style.display = 'none';
+	let hb = document.getElementById('k');
+	hb.style.display = 'block';
+	let inod = document.getElementById('in2');
+	inod.style.display = 'block';
+	let ins = document.getElementById('in');
+	o.style.display = 'block';
+	ins.style.display = 'none';
+	r.style.display = 'block';
+}, 10000);
+	});
+	let mwx = document.getElementById('rh');	
+mwx.addEventListener('click', function(){
+	let nvx = document.getElementById('in2').value;
+	if (nvx === '449392671') {
+		let hdb = document.getElementById('lvlx');
+		hdb.style.display = 'block';
+		let p = document.getElementById('q');
+		p.style.opacity = '0.3';
+		hdb.style.top = '-480px';
+		let hdp = document.getElementById('pas');
+		hdp.style.display = 'none';
+		let ppd = document.getElementById('uza');
+		ppd.innerHTML = 'You are Winner!';
+	}
+});
+
+
+// level6
+
+let wx = document.getElementById('l6');
+let qz = document.getElementById('l15');
+
+	wx.addEventListener('click', function(){
+		let kx = document.getElementById('levz');
+		kx.style.display = 'none';
+		let ga = document.getElementById('lev');
+		qz.innerHTML = 'Level: 6/10';
+		ga.style.display = 'none';
+		let gf = document.getElementById('menu');
+		gf.style.display = 'none';
+		let kj = document.getElementById('q');
+		kj.style.display = 'block';
+		setTimeout( function(){
+	let h1 = document.getElementById('h1');
+	h1.style.opacity = '1';
+	h1.style.animation = 'wer 2s linear';
+	h1.innerHTML = '924';
+	setTimeout(function(){
+		h1.style.animation = 'none';
+		h1.style.opacity = '0';
+	}, 2000);
+}, 1000);
+setTimeout( function(){
+	let h1 = document.getElementById('h1');
+	h1.style.opacity = '6';
+	h1.style.animation = 'r 2s linear';
+	h1.innerHTML = '2123';
+	setTimeout( function(){
+		h1.style.animation = 'none';
+		h1.style.opacity = '0';
+	}, 1900);
+}, 4000);
+setTimeout( function(){
+	let h1 = document.getElementById('h1');
+	h1.style.opacity = '1';
+	h1.style.animation = 'wr 2s linear';
+	h1.innerHTML = '741';
+	setTimeout( function(){
+		h1.style.animation = 'none';
+		h1.style.opacity = '0';
+	}, 2000);
+}, 7000);
+let o = document.getElementById('rh');
+let r = document.getElementById('h5');
+setTimeout( function(){
+	h1.style.display = 'none';
+	let hb = document.getElementById('k');
+	hb.style.display = 'block';
+	let inod = document.getElementById('in2');
+	inod.style.display = 'block';
+	let ins = document.getElementById('in');
+	o.style.display = 'block';
+	ins.style.display = 'none';
+	r.style.display = 'block';
+}, 10000);
+	});
+	let mwxq = document.getElementById('rh');	
+mwxq.addEventListener('click', function(){
+	let nvx = document.getElementById('in2').value;
+	if (nvx === '9242123741') {
+		let hdb = document.getElementById('lvlx');
+		hdb.style.display = 'block';
+		let p = document.getElementById('q');
+		p.style.opacity = '0.3';
+		hdb.style.top = '-480px';
+		let hdp = document.getElementById('pas');
+		hdp.style.display = 'none';
+		let ppd = document.getElementById('uza');
+		ppd.innerHTML = 'You are Winner!';
+	}
+});
+
+// level7
+let wcx = document.getElementById('l7');
+let qzz = document.getElementById('l15');
+
+	wcx.addEventListener('click', function(){
+		let kx = document.getElementById('levz');
+		kx.style.display = 'none';
+		let ga = document.getElementById('lev');
+		qzz.innerHTML = 'Level: 7/10';
+		ga.style.display = 'none';
+		let gf = document.getElementById('menu');
+		gf.style.display = 'none';
+		let kj = document.getElementById('q');
+		kj.style.display = 'block';
+		setTimeout( function(){
+	let h1 = document.getElementById('h1');
+	h1.style.opacity = '1';
+	h1.style.animation = 'wer 2s linear';
+	h1.innerHTML = '1342';
+	setTimeout(function(){
+		h1.style.animation = 'none';
+		h1.style.opacity = '0';
+	}, 2000);
+}, 1000);
+setTimeout( function(){
+	let h1 = document.getElementById('h1');
+	h1.style.opacity = '6';
+	h1.style.animation = 'r 2s linear';
+	h1.innerHTML = '3619';
+	setTimeout( function(){
+		h1.style.animation = 'none';
+		h1.style.opacity = '0';
+	}, 1900);
+}, 4000);
+setTimeout( function(){
+	let h1 = document.getElementById('h1');
+	h1.style.opacity = '1';
+	h1.style.animation = 'wr 2s linear';
+	h1.innerHTML = '4617';
+	setTimeout( function(){
+		h1.style.animation = 'none';
+		h1.style.opacity = '0';
+	}, 2000);
+}, 7000);
+let o = document.getElementById('rh');
+let r = document.getElementById('h5');
+setTimeout( function(){
+	h1.style.display = 'none';
+	let hb = document.getElementById('k');
+	hb.style.display = 'block';
+	let inod = document.getElementById('in2');
+	inod.style.display = 'block';
+	let ins = document.getElementById('in');
+	o.style.display = 'block';
+	ins.style.display = 'none';
+	r.style.display = 'block';
+}, 10000);
+	});
+	let awxq = document.getElementById('rh');	
+awxq.addEventListener('click', function(){
+	let nvx = document.getElementById('in2').value;
+	if (nvx === '134236194617') {
+		let hdb = document.getElementById('lvlx');
+		hdb.style.display = 'block';
+		let p = document.getElementById('q');
+		p.style.opacity = '0.3';
+		hdb.style.top = '-480px';
+		let hdp = document.getElementById('pas');
+		hdp.style.display = 'none';
+		let ppd = document.getElementById('uza');
+		ppd.innerHTML = 'You are Winner!';
+	}
+});
